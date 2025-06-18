cb7cf414ef51984f6f365d66279e3d339e7119aa
adamboy7 <adamboy8888@gmail.com>
2025-06-18 13:16:13 -0400

    Enable XOR-based button pulsing

--- demo/cross_loop.py
+++ demo/cross_loop.py
@@ -1,9 +1,9 @@
 import time
-from libraries.inputs import frame_delay, pulse_button
+from libraries.inputs import frame_delay, pulse_button_xor
 
 def controller_loop(stop_event, controller_states, slot):
     frame = 0
     while not stop_event.is_set():
-        pulse_button(frame, controller_states, slot, cross=True)
+        pulse_button_xor(frame, controller_states, slot, cross=True)
         frame += 1
         time.sleep(frame_delay)
